497af1e5a82142ddc3ac91a81990b2415872c852
tuanmhoang <hoangminhtuan0505@gmail.com>
2022-10-12 23:59:55 +0700

    US02-BLUE-reuse input data

--- simple-stocks/src/test/java/com/tuanmhoang/testing/tdd/service/StockServiceTest.java
+++ simple-stocks/src/test/java/com/tuanmhoang/testing/tdd/service/StockServiceTest.java
@@ -13,6 +13,12 @@ public class StockServiceTest {
 
     private StockService stockService = new StockService();
 
+    private List<SubscriptionData> inputData = Arrays.asList(
+            SubscriptionData.builder().price(12700).numberOfStocks(1400).subscriptionType(SubscriptionType.BUY).build(),
+            SubscriptionData.builder().price(11350).numberOfStocks(1000).subscriptionType(SubscriptionType.SELL).build(),
+            SubscriptionData.builder().price(13500).numberOfStocks(1000).subscriptionType(SubscriptionType.SELL).build()
+    );
+
     @Test
     public void givenStockPriceNumberOfStock_whenUserViewData_thenCalculateTotalPrice(){
         assertEquals(1000000,stockService.calculateTotalPrice(10000, 100));
@@ -21,23 +27,16 @@ public class StockServiceTest {
 
     @Test
     public void givenListOfSubscriptions_whenCalculate_thenCalculateTotalBuy(){
-        List<SubscriptionData> inputData = Arrays.asList(
-                SubscriptionData.builder().price(12700).numberOfStocks(1400).subscriptionType(SubscriptionType.BUY).build(),
-                SubscriptionData.builder().price(11350).numberOfStocks(1000).subscriptionType(SubscriptionType.SELL).build(),
-                SubscriptionData.builder().price(13500).numberOfStocks(1000).subscriptionType(SubscriptionType.SELL).build()
-        );
+
         // 12700*1400
         assertEquals(17780000, stockService.calculateTotalBasedOnSubscriptionType(inputData,SubscriptionType.BUY));
     }
 
     @Test
     public  void givenListOfSubscriptions_whenCalculate_thenCalculateTotalSell(){
-        List<SubscriptionData> inputData = Arrays.asList(
-                SubscriptionData.builder().price(12700).numberOfStocks(1400).subscriptionType(SubscriptionType.BUY).build(),
-                SubscriptionData.builder().price(11350).numberOfStocks(1000).subscriptionType(SubscriptionType.SELL).build(),
-                SubscriptionData.builder().price(13500).numberOfStocks(1000).subscriptionType(SubscriptionType.SELL).build()
-        );
+
         // 11350*1000+13500*1000
         assertEquals(24850000, stockService.calculateTotalBasedOnSubscriptionType(inputData,SubscriptionType.SELL));
     }
+
 }
