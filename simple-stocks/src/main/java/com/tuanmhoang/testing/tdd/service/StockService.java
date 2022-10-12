da72e65bf08518928de30851c4ee5748ffc272de
tuanmhoang <hoangminhtuan0505@gmail.com>
2022-10-12 23:25:30 +0700

    RED-givenStockPriceNumberOfStock_whenUserViewData_thenCalculateTotalPrice, not yet implemented

--- simple-stocks/src/main/java/com/tuanmhoang/testing/tdd/service/StockService.java
+++ simple-stocks/src/main/java/com/tuanmhoang/testing/tdd/service/StockService.java
@@ -1,4 +1,10 @@
 package com.tuanmhoang.testing.tdd.service;
 
+import java.math.BigInteger;
+
 public class StockService {
+
+    public int calculateTotalPrice(int price, int numberOfStocks){
+        return 0;
+    }
 }
--- simple-stocks/src/test/java/com/tuanmhoang/testing/tdd/service/StockServiceTest.java
+++ simple-stocks/src/test/java/com/tuanmhoang/testing/tdd/service/StockServiceTest.java
@@ -1,4 +1,16 @@
 package com.tuanmhoang.testing.tdd.service;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StockServiceTest {
+
+    private StockService stockService = new StockService();
+
+    @Test
+    public void givenStockPriceNumberOfStock_whenUserViewData_thenCalculateTotalPrice(){
+        assertEquals(1000000,stockService.calculateTotalPrice(10000, 100));
+        assertEquals(9000000,stockService.calculateTotalPrice(45000, 200));
+    }
 }
